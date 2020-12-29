501ad8688624b415013898e0453116739b2cec3b
Boris Kirov <borisasenovkirov@gmail.com>
2020-12-29 09:18:36 +0100

    second commit

---
+++ components/example.js
@@ -0,0 +1,75 @@
+import styles from '../styles/Home.module.css'
+
+export default function ExampleGrid() {
+    return <div class="cs5 ce12">
+    <grid>
+        <div class="cs1 ce1 demo">
+            <p>1-1</p>
+        </div>
+        <div class="cs2 ce4 demo">
+            <p>2-4</p>
+        </div>
+        <div class="cs5 ce9 demo">
+            <p>5-9</p>
+        </div>
+        <div class="cs10 ce12 demo">
+            <p>10-12</p>
+        </div>
+        <div class="cs4 ce6 demo">
+            <p>4-6</p>
+        </div>
+        <div class="cs7 ce8 demo">
+            <p>7-8</p>
+        </div>
+        <div class="cs1 ce1 demo">
+            <p>1-1</p>
+        </div>
+        <div class="cs2 ce2 demo">
+            <p>2-2</p>
+        </div>
+        <div class="cs3 ce3 demo">
+            <p>3-3</p>
+        </div>
+        <div class="cs4 ce4 demo">
+            <p>4-4</p>
+        </div>
+        <div class="cs5 ce5 demo">
+            <p>5-5</p>
+        </div>
+        <div class="cs6 ce6 demo">
+            <p>6-6</p>
+        </div>
+        <div class="cs7 ce7 demo">
+            <p>7-7</p>
+        </div>
+        <div class="cs8 ce8 demo">
+            <p>8-8</p>
+        </div>
+        <div class="cs9 ce9 demo">
+            <p>9-9</p>
+        </div>
+        <div class="cs10 ce10 demo">
+            <p>10-10</p>
+        </div>
+        <div class="cs11 ce11 demo">
+            <p>11-11</p>
+        </div>
+        <div class="cs12 ce12 demo">
+            <p>12-12</p>
+        </div>
+        <div class="cs5 ce8 demo">
+            <p>5-8</p>
+        </div>
+        <div class="cs1 ce9 demo">
+            <p>1-9</p>
+        </div>
+        <div class="cs4 ce12 demo">
+            <p>-12</p>
+        </div>
+        <div class="cs2 ce5 demo">
+            <p>2-5</p>
+        </div>
+    </grid>
+</div>
+  }
+  
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/fluidygridy.css'
+import '../styles/fluiditype.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
